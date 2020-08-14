5651e7aa8078cf3a16aeb3f74664b1a4fab1d069
toxa108 <antonskubiiev@gmail.com>
2020-08-14 20:56:40 +0300

    1366

---
+++ java/src/main/java/codeforces/contest/c1366/A.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class A {
+    public static void main(String[] args) {
+        FastScanner fs = new FastScanner();
+        PrintWriter pt=new PrintWriter(System.out);
+        int T = fs.nextInt();
+        for (int tt = 0; tt < T; tt++) {
+            int palka = fs.nextInt();
+            int almaz = fs.nextInt();
+            int r = (palka + almaz) / 3;
+            if (palka < almaz) {
+                if (palka < r) {
+                   pt.println(palka);
+                } else {
+                    pt.println(r);
+                }
+            } else {
+                if (almaz < r) {
+                    pt.println(almaz);
+                } else {
+                    pt.println(r);
+                }
+            }
+        }
+        pt.close();
+    }
+
+    static class FastScanner {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer("");
+
+        String next() {
+            while (!st.hasMoreTokens())
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        int[] readArray(int n) {
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) a[i] = nextInt();
+            return a;
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+}
