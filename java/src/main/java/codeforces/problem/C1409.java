aa31f6f15426fa6213415000a791ef6fcf927159
toxa108 <antonskubiiev@gmail.com>
2020-09-07 13:51:59 +0300

    some

--- java/src/main/java/codeforces/problem/C1409.java
+++ java/src/main/java/codeforces/problem/C1409.java
@@ -1,95 +1,41 @@
-package codeforces.contest.c1405;
+package codeforces.problem;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 /*
-9
-6 4
-100110
-3 2
-1?1
-3 2
-1?0
-4 4
-????
-7 4
-1?0??1?
-10 10
-11??11??11
-4 2
-1??1
-4 4
-?0?0
-6 2
-????00
+https://codeforces.com/problemset/problem/1409/C
+5
+2 1 49
+5 20 50
+6 20 50
+5 3 8
+9 13 22
 
-1
-5 2
-1??10
-
-1
-3 2
-1?0
-
-1
-12 6
-11?00?0?1010
-
-1
-7 4
-1?0??1?
  */
-public class Ctask {
+public class C1409 {
 
     static void solve() {
         int n = FS.nextInt();
-        int k = FS.nextInt();
-        char[] c = FS.next().toCharArray();
-        for (int i = 0; i < k; i++) {
-            char set = 0;
-            for (int j = i; j < n; j += k) {
-                if (set != 0) {
-                    if (c[j] != set && c[j] != '?') {
-                        FS.pt.println("NO");
-                        return;
-                    }
-                } else {
-                    if (c[j] != '?')
-                        set = c[j];
-                }
+        int x = FS.nextInt();
+        int y = FS.nextInt();
+
+        int diff = y - x;
+        for (int delta = 1; delta <= diff; ++delta) {
+            if (diff % delta != 0) continue;
+            if (diff / delta + 1 > n) continue;
+            int k = Math.min((y - 1) / delta, n - 1);
+            int a0 = y - k * delta;
+            for (int i = 0; i < n; ++i) {
+                FS.pt.print(a0 + i * delta + " ");
             }
-            if (set != 0) {
-                for (int j = i; j < n; j += k) {
-                    c[j] = set;
-                }
-            }
+            FS.pt.println();
+            break;
         }
-
-        int sum = 0;
-        int other = 0;
-        for (int i = 0; i < k; i++) {
-            if (c[i] == '1')
-                sum++;
-            else if (c[i] == '0')
-                sum--;
-            else
-                other++;
-        }
-
-        if (Math.abs(sum) > other) {
-            FS.pt.println("NO");
-            return;
-        }
-
-        FS.pt.println("YES");
-        return;
     }
-
 
     public static void main(String[] args) {
         int T = FS.nextInt();
@@ -97,6 +43,16 @@ public class Ctask {
             solve();
         }
         FS.pt.close();
+    }
+
+    static class Pair<T, N> {
+        T i;
+        N j;
+
+        public Pair(T i, N j) {
+            this.i = i;
+            this.j = j;
+        }
     }
 
     static class FS {
@@ -124,14 +80,18 @@ public class Ctask {
             return a;
         }
 
-        static long[] readArrayL(int n) {
-            long[] a = new long[n];
-            for (int i = 0; i < n; i++) a[i] = nextLong();
+        static int[][] read2Array(int m, int n) {
+            int[][] a = new int[m][n];
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    a[i][j] = nextInt();
+                }
+            }
             return a;
         }
 
-        static int[][] read2Array(int m, int n) {
-            int[][] a = new int[m][n];
+        static long[][] read2ArrayL(int m, int n) {
+            long[][] a = new long[m][n];
             for (int i = 0; i < m; i++) {
                 for (int j = 0; j < n; j++) {
                     a[i][j] = nextInt();
@@ -156,7 +116,16 @@ public class Ctask {
                 }
                 pt.println();
             }
-            pt.println();
+        }
+
+        static void print2ArrL(long[][] arr, int m, int n) {
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    pt.print(arr[i][j]);
+                    pt.print(" ");
+                }
+                pt.println();
+            }
         }
 
         static void close() {
