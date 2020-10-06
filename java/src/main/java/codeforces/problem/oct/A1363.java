e937ef1d1868dd8d51c9f7cd3c7bc8ca30558ae1
1ou <antonskubiiev@gmail.com>
2020-10-06 15:54:27 +0300

    some

--- java/src/main/java/codeforces/problem/oct/A1363.java
+++ java/src/main/java/codeforces/problem/oct/A1363.java
@@ -1,19 +1,57 @@
-package codeforces;
+package codeforces.problem.oct;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.util.StringTokenizer;
+import java.util.regex.Pattern;
 
 /*
-https://codeforces.com/problemset/problem/?/?
+https://codeforces.com/problemset/problem/1363/A
+7
+4 2
+19 11 13 15
+1 1
+999
+1 1
+1000
+2 1
+51 50
+2 2
+51 50
+3 3
+101 102 103
+9 7
+11 14 1 6 3 12 3 20 16
  */
-public class aTemplate {
+public class A1363 {
 
     static void solve() {
         int n = FS.nextInt();
-
+        int x = FS.nextInt();
+        int[] arr = FS.readArray(n);
+        int od = 0;
+        int even = 0;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] % 2 == 0) {
+                even++;
+            } else {
+                od++;
+            }
+        }
+        boolean ok = false;
+        for (int odd = 0; odd <= x; odd++) {
+            int evan = x - odd;
+            if (odd <= od && evan <= even && odd % 2 != 0) {
+                ok = true;
+            }
+        }
+        if (ok) {
+            FS.pt.println("YES");
+        } else {
+            FS.pt.println("NO");
+        }
     }
 
     public static void main(String[] args) {
