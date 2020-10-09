a96744535fbd8ff6820ad39f729693bb41c45076
1ou <antonskubiiev@gmail.com>
2020-10-09 08:55:45 +0300

    some

---
+++ java/src/main/java/codeforces/problem/oct/B706.java
@@ -0,0 +1,181 @@
+package codeforces.problem.oct;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.*;
+import java.util.stream.Stream;
+
+/*
+https://codeforces.com/problemset/problem/706/B
+5
+3 10 8 6 11
+5
+1
+10
+3
+11
+9
+
+6
+7 7 7 7 8 8
+1
+7
+ */
+public class B706 {
+    public static int runBinarySearchIteratively2(long[] sortedArray, long key, int low, int high) {
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (sortedArray[mid] < key) {
+                low = mid + 1;
+            } else if (sortedArray[mid] > key) {
+                high = mid - 1;
+            } else if (sortedArray[mid] == key) {
+                return mid + 1;
+            }
+        }
+        return Math.max(low, 0);
+    }
+
+    public static int runBinarySearchIteratively(long[] sortedArray, long key, int low, int high) {
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (sortedArray[mid] < key) {
+                low = mid + 1;
+            } else if (sortedArray[mid] > key) {
+                high = mid - 1;
+            } else if (sortedArray[mid] == key) {
+                while (mid < high - 1000 && sortedArray[mid + 999] == sortedArray[mid]) mid+=999;
+                while (mid < high && sortedArray[mid + 1] == sortedArray[mid]) mid++;
+                return mid + 1;
+            }
+        }
+        return Math.max(low, 0);
+    }
+
+    static void solve() {
+        int n = FS.nextInt();
+        long[] x = FS.readArrayL(n);
+        int q = FS.nextInt();
+        Arrays.sort(x);
+        for (int i = 0; i < q; i++) {
+            long m = FS.nextLong();
+            FS.pt.println(runBinarySearchIteratively(x, m, 0, n - 1));
+        }
+    }
+
+    public static void main(String[] args) {
+//        int T = FS.nextInt();
+//        for (int tt = 0; tt < T; tt++) {
+        solve();
+//        }
+        FS.pt.close();
+    }
+
+    static class FS {
+        private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        private static StringTokenizer st = new StringTokenizer("");
+        static PrintWriter pt = new PrintWriter(System.out);
+
+        static String next() {
+            while (!st.hasMoreTokens())
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            return st.nextToken();
+        }
+
+        static int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        static int[] readArray(int n) {
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) a[i] = nextInt();
+            return a;
+        }
+
+        static int[][] read2Array(int m, int n) {
+            int[][] a = new int[m][n];
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    a[i][j] = nextInt();
+                }
+            }
+            return a;
+        }
+
+        static long[][] read2ArrayL(int m, int n) {
+            long[][] a = new long[m][n];
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    a[i][j] = nextInt();
+                }
+            }
+            return a;
+        }
+
+        void printArr(long[] arr) {
+            for (long value : arr) {
+                pt.print(value);
+                pt.print(" ");
+            }
+            pt.println();
+        }
+
+        static long[] readArrayL(int n) {
+            long[] a = new long[n];
+            for (int i = 0; i < n; i++) a[i] = nextInt();
+            return a;
+        }
+
+        static void printArr(int[] arr) {
+            for (int value : arr) {
+                pt.print(value);
+                pt.print(" ");
+            }
+            pt.println();
+        }
+
+        static void printArrL(int[] arr) {
+            for (int value : arr) {
+                pt.print(value);
+                pt.print(" ");
+            }
+            pt.println();
+        }
+
+        static void print2Arr(int[][] arr, int m, int n) {
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    pt.print(arr[i][j]);
+                    pt.print(" ");
+                }
+                pt.println();
+            }
+            pt.println();
+        }
+
+        static void print2Arr(long[][] arr, int m, int n) {
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    pt.print(arr[i][j]);
+                    pt.print(" ");
+                }
+                pt.println();
+            }
+            pt.println();
+        }
+
+        static void close() {
+            pt.close();
+        }
+
+        static long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+}
