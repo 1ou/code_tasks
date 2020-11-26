87d876071e5c3476823c8d7b3ebda14dea80ddaa
toxa108 <antonskubiiev@gmail.com>
2020-11-26 08:47:27 +0300

    §

---
+++ java/src/main/java/codeforces/problem/nov/TaskB_1427.java
@@ -0,0 +1,219 @@
+package codeforces.problem.nov;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.*;
+
+/*
+https://codeforces.com/problemset/problem/1427/B
+2
+5 2
+WLWLL
+6 5
+LLLWWL
+
+1
+15 5
+WWWLLLWWWLLLWWW
+ */
+public class TaskB_1427 {
+
+    static void solve() {
+        int n = FS.nextInt();
+        int k = FS.nextInt();
+        int tmpK = k;
+        char[] c = FS.next().toCharArray();
+
+        List<Integer> gaps = new ArrayList<>();
+        int wins = 0;
+        int cnt = 0;
+        int winstreak = 0;
+        boolean streak = false;
+        for (int i = 0; i < n; i++) {
+
+            if (i > 0 && c[i] == 'W' && c[i - 1] == 'W') {
+                streak = true;
+            }
+
+            if (c[i] == 'W') {
+                wins++;
+            }
+
+            if (c[i] == 'L' && streak) {
+                streak = false;
+                winstreak++;
+            }
+
+            if (cnt == 0 && c[i] == 'L') {
+                cnt++;
+                continue;
+            }
+
+            if (cnt > 0 && c[i] == 'W') {
+                gaps.add(cnt);
+                cnt = 0;
+            }
+        }
+
+        int filledGaps = 0;
+        gaps.sort(Comparator.comparingInt(it -> it));
+        for (int i = 0; i < gaps.size(); i++) {
+            if (k >= gaps.get(i)) {
+                filledGaps++;
+                k -= gaps.get(i);
+            }
+        }
+
+        FS.pt.println(2 * (tmpK + wins) - winstreak + filledGaps);
+    }
+
+    public static void main(String[] args) {
+        int T = FS.nextInt();
+        for (int tt = 0; tt < T; tt++) {
+            solve();
+        }
+        FS.pt.close();
+    }
+
+    static class FS {
+        private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        private static StringTokenizer st = new StringTokenizer("");
+        static PrintWriter pt = new PrintWriter(System.out);
+
+        static String next() {
+            while (!st.hasMoreTokens())
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            return st.nextToken();
+        }
+
+        static int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        static int[] readArray(int n) {
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) a[i] = nextInt();
+            return a;
+        }
+
+        static int[][] read2Array(int n, int m) {
+            int[][] a = new int[n][m];
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    a[i][j] = nextInt();
+                }
+            }
+            return a;
+        }
+
+        static long[][] read2ArrayL(int n, int m) {
+            long[][] a = new long[n][m];
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    a[i][j] = nextInt();
+                }
+            }
+            return a;
+        }
+
+        void printArr(long[] arr) {
+            for (long value : arr) {
+                pt.print(value);
+                pt.print(" ");
+            }
+            pt.println();
+        }
+
+        static long[] readArrayL(int n) {
+            long[] a = new long[n];
+            for (int i = 0; i < n; i++) a[i] = nextInt();
+            return a;
+        }
+
+        static void printArr(int[] arr) {
+            for (int value : arr) {
+                pt.print(value);
+                pt.print(" ");
+            }
+            pt.println();
+        }
+
+        static void printArrL(int[] arr) {
+            for (int value : arr) {
+                pt.print(value);
+                pt.print(" ");
+            }
+            pt.println();
+        }
+
+        static void print2Arr(int[][] arr, int n, int m) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    pt.print(arr[i][j]);
+                    pt.print(" ");
+                }
+                pt.println();
+            }
+            pt.println();
+        }
+
+        static void print2Arr(long[][] arr, int n, int m) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    pt.print(arr[i][j]);
+                    pt.print(" ");
+                }
+                pt.println();
+            }
+            pt.println();
+        }
+
+        static List<Long> readListLong(int n) {
+            List<Long> list = new ArrayList<>(n);
+            for (int i = 0; i < n; i++) {
+                list.add(nextLong());
+            }
+            return list;
+        }
+
+        static List<Integer> readListInt(int n) {
+            List<Integer> list = new ArrayList<>(n);
+            for (int i = 0; i < n; i++) {
+                list.add(nextInt());
+            }
+            return list;
+        }
+
+        static <T> void printList(List<T> list) {
+            for (T value : list) {
+                pt.print(value);
+                pt.print(" ");
+            }
+            pt.println();
+        }
+
+        static void close() {
+            pt.close();
+        }
+
+        static long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+
+    static class Pair<T, K> {
+        T first;
+        K second;
+
+        public Pair(T first, K second) {
+            this.first = first;
+            this.second = second;
+        }
+    }
+}
