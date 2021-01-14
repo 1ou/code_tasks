95379c451b97c639d53f8300be227a8831baa428
1ou <antonskubiiev@gmail.com>
2021-01-15 01:10:31 +0300

    a

---
+++ java/src/main/java/codeforces/problem/jan/Task1473_B.java
@@ -0,0 +1,303 @@
+package codeforces.problem.jan;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.StringTokenizer;
+
+/*
+https://codeforces.com/problemset/problem/1473/B
+ */
+public class Task1473_B {
+
+    static void solve() {
+        String s = FS.next();
+        String t = FS.next();
+
+        String bigger;
+        String less;
+
+        if (s.length() < t.length()) {
+            bigger = t;
+            less = s;
+        } else {
+            bigger = s;
+            less = t;
+        }
+
+        if (bigger.contains(less)) {
+            if (bigger.substring(bigger.length() - less.length()).equals(less)) {
+                boolean tt = true;
+                int indexLess = 0;
+                for (int i = 0; i < bigger.length(); i++) {
+                    if (bigger.charAt(i) != less.charAt(indexLess)) {
+                        tt = false;
+                        break;
+                    }
+                    indexLess++;
+                    if (indexLess == less.length()) {
+                        indexLess = 0;
+                    }
+                }
+                boolean containSameLatter = true;
+                if (bigger.length() > 1) {
+                    for (int i = 1; i < bigger.length(); i++) {
+                        if (bigger.charAt(i - 1) != bigger.charAt(i)) {
+                            containSameLatter = false;
+                            break;
+                        }
+                    }
+                }
+                if (less.length() > 1) {
+                    for (int i = 1; i < less.length(); i++) {
+                        if (less.charAt(i - 1) != less.charAt(i)) {
+                            containSameLatter = false;
+                            break;
+                        }
+                    }
+                }
+
+                if (bigger.charAt(0) != less.charAt(0)) {
+                    containSameLatter = false;
+                }
+                if (!tt && !containSameLatter) {
+                    FS.pt.println("-1");
+                    return;
+                }
+
+                for (int i = 0; i < NumberTheory.lcm(bigger.length(), less.length()) / less.length(); i++) {
+                    FS.pt.print(less);
+                }
+                FS.pt.println();
+                return;
+            }
+        }
+
+        FS.pt.println("-1");
+    }
+
+    public static void main(String[] args) {
+        int T = FS.nextInt();
+        for (int tt = 0; tt < T; tt++) {
+            solve();
+        }
+        FS.pt.close();
+    }
+
+    static class FS {
+        private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        private static StringTokenizer st = new StringTokenizer("");
+        static PrintWriter pt = new PrintWriter(System.out);
+
+        static String next() {
+            while (!st.hasMoreTokens())
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            return st.nextToken();
+        }
+
+        static int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        static int[] readArray(int n) {
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) a[i] = nextInt();
+            return a;
+        }
+
+        static double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        static int[][] read2Array(int n, int m) {
+            int[][] a = new int[n][m];
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    a[i][j] = nextInt();
+                }
+            }
+            return a;
+        }
+
+        static long[][] read2ArrayL(int n, int m) {
+            long[][] a = new long[n][m];
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    a[i][j] = nextLong();
+                }
+            }
+            return a;
+        }
+
+        void printArr(long[] arr) {
+            for (long value : arr) {
+                pt.print(value);
+                pt.print(" ");
+            }
+            pt.println();
+        }
+
+        static long[] readArrayL(int n) {
+            long[] a = new long[n];
+            for (int i = 0; i < n; i++) a[i] = nextLong();
+            return a;
+        }
+
+        static void printArr(int[] arr) {
+            for (int value : arr) {
+                pt.print(value);
+                pt.print(" ");
+            }
+            pt.println();
+        }
+
+        static void printArrL(int[] arr) {
+            for (int value : arr) {
+                pt.print(value);
+                pt.print(" ");
+            }
+            pt.println();
+        }
+
+        static void print2Arr(int[][] arr, int n, int m) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    pt.print(arr[i][j]);
+                    pt.print(" ");
+                }
+                pt.println();
+            }
+            pt.println();
+        }
+
+        static void print2Arr(long[][] arr, int n, int m) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    pt.print(arr[i][j]);
+                    pt.print(" ");
+                }
+                pt.println();
+            }
+            pt.println();
+        }
+
+        static List<Long> readListLong(int n) {
+            List<Long> list = new ArrayList<>(n);
+            for (int i = 0; i < n; i++) {
+                list.add(nextLong());
+            }
+            return list;
+        }
+
+        static List<Integer> readListInt(int n) {
+            List<Integer> list = new ArrayList<>(n);
+            for (int i = 0; i < n; i++) {
+                list.add(nextInt());
+            }
+            return list;
+        }
+
+        static <T> void printList(List<T> list) {
+            for (T value : list) {
+                pt.print(value);
+                pt.print(" ");
+            }
+            pt.println();
+        }
+
+        static void close() {
+            pt.close();
+        }
+
+        static long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+
+    static class Mat {
+        public static long pow(long a, long exp) {
+            return (long) Math.pow(a, exp);
+        }
+    }
+
+    static class NumberTheory {
+        public static long gcd(long a, long b) {
+            while (b != 0) {
+                long tmp = a % b;
+                a = b;
+                b = tmp;
+            }
+            return a;
+        }
+
+        public static long lcm(long a, long b) {
+            return a * b / gcd(a, b);
+        }
+
+        public static List<Long> primeFactors(long n) {
+            List<Long> ans = new ArrayList<>();
+            while (n % 2 == 0) {
+                ans.add(2L);
+                n /= 2;
+            }
+
+            // n must be odd at this point.  So we can
+            // skip one element (Note i = i + 2)
+            for (long i = 3; i <= Math.sqrt(n); i += 2) {
+                // While i divides n, print i and divide n
+                while (n % i == 0) {
+                    ans.add(i);
+                    n /= i;
+                }
+            }
+
+            // This condition is to handle the case when
+            // n is a prime number greater than 2
+            if (n > 2) {
+                ans.add(n);
+            }
+            return ans;
+        }
+
+        static long fact(long n) {
+            if (n == 0 || n == 1) return 1L;
+            long r = 1;
+            for (int i = 1; i <= n; i++) {
+                r *= i;
+            }
+            return r;
+        }
+    }
+
+    static class Pair<T, K> {
+        T first;
+        K second;
+
+        public Pair(T first, K second) {
+            this.first = first;
+            this.second = second;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Pair<?, ?> pair = (Pair<?, ?>) o;
+            return Objects.equals(first, pair.first) &&
+                    Objects.equals(second, pair.second);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(first, second);
+        }
+    }
+}
